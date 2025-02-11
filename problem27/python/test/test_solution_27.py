a6dd7738d91961c60d746d1826e8f81594ce8f63
Cameron Davies <camerondavies@Camerons-MacBook-Pro.local>
2025-02-10 20:57:28 -0700

    format(python/problem27): formatting

--- problem27/python/test/test_solution_27.py
+++ problem27/python/test/test_solution_27.py
@@ -2,27 +2,26 @@ from problem27.python.solution import Solution
 
 
 class TestSolution27:
-
     def test_case_one(self):
         solution = Solution()
-        nums = [3,2,2,3]
-        expected_nums = [2,2]
+        nums = [3, 2, 2, 3]
+        expected_nums = [2, 2]
         val = 3
         val_count = solution.removeElement(nums, val)
         assert val_count == len(expected_nums)
         nums[:] = nums[:val_count]
         nums.sort()
-        for i in range(len(expected_nums)): 
+        for i in range(len(expected_nums)):
             assert nums[i] == expected_nums[i]
 
     def test_case_two(self):
         solution = Solution()
-        nums = [0,1,2,2,3,0,4,2]
-        expected_nums = [0,0,1,3,4]
+        nums = [0, 1, 2, 2, 3, 0, 4, 2]
+        expected_nums = [0, 0, 1, 3, 4]
         val = 2
         val_count = solution.removeElement(nums, val)
         assert val_count == len(expected_nums)
         nums[:] = nums[:val_count]
         nums.sort()
-        for i in range(len(expected_nums)): 
+        for i in range(len(expected_nums)):
             assert nums[i] == expected_nums[i]
