bfec1648b86ce9c086ea4e2ed30c471e94c01b59
Cameron Davies <camerondavies@Camerons-MacBook-Pro.local>
2025-03-14 09:37:26 -0600

    fix(utils): add some boilerplate to the test method

--- utils/setup_python.sh
+++ utils/setup_python.sh
@@ -11,3 +11,5 @@ touch "problem${problem_number}/python/README.md"
 touch "problem${problem_number}/python/solution.py"
 touch "problem${problem_number}/python/test/test_solution${problem_number}.py"
 
+echo "from problem${problem_number}.python.solution import Solution\n\n" >> "problem${problem_number}/python/test/test_solution${problem_number}.py"
+echo "class TestSolution${problem_number}:\n    def test_case_one(self):\n" >> "problem${problem_number}/python/test/test_solution${problem_number}.py"
